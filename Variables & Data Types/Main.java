4dda2fe4d78212d7e26db403fc91f88a5bfd98ea
ayushporwal01 <porwalayush953@gmail.com>
2025-05-27 11:10:23 +0530

    Day-2

---
+++ Variables & Data Types/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        String name = "Ayush";
+        int age = 20;
+        float height = 5.9f;
+        double weight = 55.5;
+        boolean isStudent = true;
+        char grade = 'A';
+        
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height);
+        System.out.println("Weight: " + weight);
+        System.out.println("Is Student: " + isStudent);
+        System.out.println("Grade: " + grade);
+    }    
+}
